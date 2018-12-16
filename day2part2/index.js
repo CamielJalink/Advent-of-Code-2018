1164b600e56d9245f9302954a5e1ce9dc19aee1f
Camiel <camiel.jalink@gmail.com>
2018-12-16 13:24:48 +0100

    Day 2 finished

---
+++ day2part2/index.js
@@ -0,0 +1,33 @@
+"use strict";
+// Unlike the previous three exercises, this one is built using typescript.
+// Start with node index.js  (instead of npm start in the previous examples)
+exports.__esModule = true;
+var fs_1 = require("fs");
+fs_1.readFile("input.txt", "utf8", advent);
+function advent(error, input) {
+    var inputArray = input.split("\n");
+    // vars in for loops nog naar const omschrijven
+    for (var i = 0; i < inputArray.length - 1; i++) {
+        for (var j = i + 1; j < inputArray.length; j++) {
+            var diffNumber = 0;
+            for (var k = 0; k < inputArray[i].length; k++) {
+                if (inputArray[i][k] !== inputArray[j][k]) {
+                    diffNumber++;
+                }
+                if (diffNumber > 1) {
+                    break;
+                }
+            }
+            if (diffNumber === 1) {
+                // We found em, now find the corresponding characters in both strings.
+                var resultString = "";
+                for (var l = 0; l < inputArray[i].length; l++) {
+                    if (inputArray[i][l] === inputArray[j][l]) {
+                        resultString += inputArray[i][l];
+                    }
+                }
+                return (console.log("Houston, we got 'em" + " " + resultString));
+            }
+        }
+    }
+}
