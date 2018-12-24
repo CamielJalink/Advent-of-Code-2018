c4c0091a020f4f7ac40b53abefcab18378cfd31a
Camiel Jalink <camiel@summit.nl>
2018-12-24 11:55:10 +0100

    Initial classes added for day 6

--- day6/index.js
+++ day6/index.js
@@ -3,10 +3,81 @@ exports.__esModule = true;
 // Advent of code day 6
 var fs_1 = require("fs");
 fs_1.readFile("input.txt", "utf8", advent);
-// Main function called when the program is started
+// deze class is nodig want: ik wil weten hoeveel punten hij 'bij zijn vlak' heeft. 
+var Coordinate = /** @class */ (function () {
+    function Coordinate(stringCoordinate) {
+        this.x = 0;
+        this.y = 0;
+        this.isInfinite = false;
+        this.positionsList = [];
+        var splitArray = stringCoordinate.split(", ");
+        this.x = parseInt(splitArray[0]);
+        this.y = parseInt(splitArray[1]);
+    }
+    return Coordinate;
+}());
+var Position = /** @class */ (function () {
+    function Position(x, y) {
+        this.x = 0;
+        this.y = 0;
+        this.closestCoordinate = new Coordinate("0, 0");
+        this.x = x;
+        this.y = y;
+    }
+    Position.prototype.setClosestCoordinate = function (coordinate) {
+        this.closestCoordinate = coordinate;
+    };
+    return Position;
+}());
 function advent(error, input) {
-    console.log(input);
-    var inputArray = [];
+    var inputArray = input.split("\n");
+    var coordinates = [];
+    inputArray.forEach(function (stringCoord) {
+        coordinates.push(new Coordinate(stringCoord));
+    });
+    // Determine relevant playing field:
+    var minX = coordinates[0].x, maxX = coordinates[0].y;
+    var minY = coordinates[0].y, maxY = coordinates[0].y;
+    for (var i = 1; i < coordinates.length; i++) {
+        if (coordinates[i].x < minX) {
+            minX = coordinates[i].x;
+        }
+        if (coordinates[i].x > maxX) {
+            maxX = coordinates[i].x;
+        }
+        if (coordinates[i].y < minY) {
+            minY = coordinates[i].y;
+        }
+        if (coordinates[i].y > maxY) {
+            maxY = coordinates[i].y;
+        }
+    }
+    var positionArray = [];
+    for (var i = minX; i < maxX + 1; i++) {
+        for (var j = minY; j < maxY + 1; j++) {
+            var isCoordinate = false;
+            for (var k = 0; k < coordinates.length; k++) {
+                if (i === coordinates[k].x && j === coordinates[k].y) {
+                    isCoordinate = true;
+                    break;
+                }
+            }
+            if (!isCoordinate) {
+                positionArray.push(new Position(i, j));
+            }
+        }
+    }
+    console.log(positionArray);
+    for (var i = 0; i < positionArray.length; i++) {
+        var closestCoordinate = coordinates[0];
+        for (var j = 0; j < coordinates.length; j++) {
+            // this still needs work.
+        }
+        positionArray[i].setClosestCoordinate(closestCoordinate);
+    }
+}
+function determineDistance(x1, y1, x2, y2) {
+    return Math.abs(x2 - x1) + Math.abs(y2 - y1);
 }
 // Idee: Een class coordinates, die een x en een y coordinaat heeft.
 // --> Een class grid met een x en een y die zelf berekent bij welk coordinaat hij hoort,
--- day6/index.ts
+++ day6/index.ts
@@ -1,12 +1,96 @@
 // Advent of code day 6
 import { readFile } from "fs";
+import { stringify } from "querystring";
 readFile("input.txt", "utf8", advent);
- 
-// Main function called when the program is started
-function advent(error: Error, input: string){
-    console.log(input);
 
-    let inputArray: string[] = [];
+// deze class is nodig want: ik wil weten hoeveel punten hij 'bij zijn vlak' heeft. 
+class Coordinate {
+    x: number = 0;
+    y: number = 0;
+    isInfinite: boolean = false; 
+    positionsList: Position[] = []; 
+
+    constructor(stringCoordinate:string){
+        let splitArray: string[] = stringCoordinate.split(", ");
+        this.x = parseInt(splitArray[0]);
+        this.y = parseInt(splitArray[1]);
+    }
+}
+
+
+class Position {
+    x: number = 0;
+    y: number = 0;
+    closestCoordinate: Coordinate = new Coordinate("0, 0");
+
+    constructor(x: number, y: number){
+        this.x = x;
+        this.y = y;
+    }
+
+    setClosestCoordinate(coordinate: Coordinate){
+        this.closestCoordinate = coordinate; 
+    }
+}
+
+
+function advent(error: Error, input: string){
+    
+    let inputArray: string[] = input.split("\n");
+    let coordinates: Coordinate[] = [];
+    inputArray.forEach(stringCoord => {
+        coordinates.push(new Coordinate(stringCoord));
+    })
+
+    // Determine relevant playing field:
+    let minX: number = coordinates[0].x, maxX: number = coordinates[0].y;
+    let minY: number = coordinates[0].y, maxY: number = coordinates[0].y;
+    for(let i = 1; i < coordinates.length; i++){
+        if(coordinates[i].x < minX){
+            minX = coordinates[i].x;
+        }
+        if(coordinates[i].x > maxX){
+            maxX = coordinates[i].x;
+        }
+        if(coordinates[i].y < minY){
+            minY = coordinates[i].y;
+        }
+        if(coordinates[i].y > maxY){
+            maxY = coordinates[i].y;
+        }
+    }
+
+    let positionArray: Position[] = [];
+    for(let i = minX; i < maxX + 1; i++){
+        for(let j = minY; j < maxY + 1; j++){
+            let isCoordinate: boolean = false;
+            for(let k = 0; k < coordinates.length; k++){
+                if(i === coordinates[k].x && j === coordinates[k].y){
+                    isCoordinate = true;
+                    break; 
+                }
+            }
+
+            if(!isCoordinate){
+                positionArray.push(new Position(i,j));
+            }
+        }
+    }
+
+    console.log(positionArray);
+
+    for(let i = 0; i < positionArray.length; i++){
+        let closestCoordinate: Coordinate = coordinates[0];
+        for(let j = 0; j < coordinates.length; j++){
+            // this still needs work.
+        }
+        positionArray[i].setClosestCoordinate(closestCoordinate);
+    }
+}
+
+
+function determineDistance(x1:number, y1:number, x2:number, y2:number){
+    return Math.abs(x2-x1) + Math.abs(y2-y1);
 }
 
 // Idee: Een class coordinates, die een x en een y coordinaat heeft.
